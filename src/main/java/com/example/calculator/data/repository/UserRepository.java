c4650aa885b321f07149009f56d76a47e5fff3f5
GBozhinov <g_bozhinov@outlook.com>
2023-02-26 06:49:01 +0200

    Manage users page, update request done

--- src/main/java/com/example/calculator/data/repository/UserRepository.java
+++ src/main/java/com/example/calculator/data/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
@@ -14,7 +15,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
 
-    @Modifying
-    @Query("UPDATE UserEntity u SET u.username = :username, u.firstName = :firstName, u.lastName = :lastName WHERE u.id = :id")
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE UserEntity u SET u.username = :username, u.firstName = :firstName, u.lastName = :lastName WHERE u.id = :id")
     void updateUser(@Param(value = "id") long id, @Param(value = "username") String username, @Param(value = "firstName") String firstName, @Param(value = "lastName") String lastName);
 }
